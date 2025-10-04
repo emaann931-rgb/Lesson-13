8061e0f7d5f6cf183fb07630152661827f565c63
Noor13456 <noorgrps@gmail.com>
2025-10-04 17:48:10 +0500

    1

---
+++ activity 1.py
@@ -0,0 +1,31 @@
+#To print the word "Welcome to class" using string concatenation
+a="Welcome"
+b="to"
+c="class"
+result= (a+"  "+b+" "+c)
+print(result)
+
+#To print the word "Welcome to class" using string concatenation
+print("Welcome"+"  "+"to"+" "+"class")
+
+# To print “ Noor Ul Emaan“ without using variables
+print("Noor"+"  "+"ul"+" "+"Emaan")
+
+
+#Ask the user for their first and last name and print them as one name using string concatenation
+a=input("what is your First name?")
+b=input("what is your Last name?")
+print(a+" "+b)
+
+#Ask the user of different length of triangle and find the perimeter of it
+a=input("Enter side 1")
+b=input("Enter side 1")
+c=input("Enter side 1e")
+print(a+b+c)
+
+
+
+a=int(input("Enter side 1"))
+a=int(input("Enter side 2"))
+a=int(input("Enter side 3"))
+print(a+b+c)
